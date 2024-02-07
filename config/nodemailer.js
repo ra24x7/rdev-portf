9b8fad793e357a7a1307fecfc17a136e4ab39d83
Rajat Agrawal <r.agrawal247@gmail.com>
2024-02-07 15:16:37 +0530

    Fix: Env file error

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer"
 import dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' });
+dotenv.config({ path: '.env' });
 
 const email = process.env.EMAIL;
 const pass = process.env.EMAIL_PASS;
